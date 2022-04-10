4061abcaaec3efb681f38177f2540e228a3d6de3
pkstDev <William27060827@hotmail.com>
2022-04-10 12:34:29 +0800

    Bump version to 1.0.1
    * Use UTF-8 encoding for files
    * Add /exporticons all

--- src/main/java/top/gregtao/iconrenderer/IconRenderer.java
+++ src/main/java/top/gregtao/iconrenderer/IconRenderer.java
@@ -1,32 +1,15 @@
 package top.gregtao.iconrenderer;
 
-import com.mojang.brigadier.arguments.StringArgumentType;
 import net.fabricmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
-import org.quiltmc.qsl.command.api.client.ClientCommandManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import top.gregtao.iconrenderer.utils.FileHelper;
-
-import java.io.IOException;
+import top.gregtao.iconrenderer.commands.CommandInit;
 
 public class IconRenderer implements ModInitializer {
 	public static final Logger logger = LoggerFactory.getLogger("Icon Renderer");
-
 	@Override
 	public void onInitialize(ModContainer mod) {
-		ClientCommandManager.DISPATCHER.register(ClientCommandManager.literal("exporticons")
-				.then(ClientCommandManager.argument("modid", StringArgumentType.string())
-						.executes((context) -> {
-							String modId = context.getArgument("modid", String.class);
-							try {
-								new FileHelper(modId);
-							} catch (IOException e) {
-								e.printStackTrace();
-							}
-							return 1;
-						})
-				)
-		);
+		CommandInit.registerCommands();
 	}
 }
